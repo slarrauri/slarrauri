9792fd9b65031c34c0eed5c5ea538922de0aec6a
sebastian <slarrauri@gmail.com>
2020-09-02 09:40:16 -0300

    refactor random avatar

--- js/app.js
+++ js/app.js
@@ -2,20 +2,35 @@ window.onload = function() {
     slarrauri();
   };
 
+
+/**
+ * Init App
+ */
 function slarrauri(){
     console.log('Oh! LALALA!!! Señor Frances');
     randomAvatar();
 }
 
-
+/**
+ * Randomly get the avatar image.
+ */
 function randomAvatar(){
 
     var selectAvatar =  Math.floor((2 - 0) * Math.random());
-    console.log(selectAvatar);
-    if (selectAvatar == 0) {
-        document.getElementById("slarrauri-index-avatar").src = "./assets/vista_derecha.png";
-    } else {
-        document.getElementById("slarrauri-index-avatar").src = "./assets/vista_izquierda.png";
+    
+    switch (selectAvatar) {
+        case 0:
+            document.getElementById("slarrauri-index-avatar").src 
+            = "./assets/vista_derecha.png";
+            break;
+        case 1:
+                document.getElementById("slarrauri-index-avatar").src 
+                = "./assets/vista_izquierda.png";
+                break;
+        default:
+            document.getElementById("slarrauri-index-avatar").src 
+            = "./assets/vista_izquierda.png";
+            break;
     }
     
 
