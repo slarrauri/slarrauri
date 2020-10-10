b48b5a91cb82b73d639804a1c38cf0f1add39cca
sebastian <slarrauri@gmail.com>
2020-10-10 14:50:36 -0300

    add random simpsons chapter

--- js/app.js
+++ js/app.js
@@ -9,6 +9,13 @@ window.onload = function() {
 function slarrauri(){
     console.log('Oh! LALALA!!! Señor Frances');
     randomAvatar();
+    
+    if (window.location.pathname == '/random_simpsons_chapter.html') {
+        randomSimpsonsChapter();
+    }
+    document.getElementById("slarrauri-random-simpson-chapter-other").onclick 
+        = function() {randomSimpsonsChapter()};
+   
 }
 
 /**
@@ -33,5 +40,71 @@ function randomAvatar(){
             break;
     }
     
+    
+
+}
+
+/**
+ * Random Simpsons Chapter
+ */
+function randomSimpsonsChapter(){
+
+    var season  = Math.floor(Math.random() * 31) + 1;
+    var chapter = 0;
+
+    
+    switch (season) {
+        case 1:
+            chapter =  Math.floor(Math.random() * 13) + 1;        
+            break;
+        case 3:
+            chapter =  Math.floor(Math.random() * 24) + 1;  
+            break;
+        case 6:
+            chapter =  Math.floor(Math.random() * 25) + 1;  
+            break;
+        case 7:
+            chapter =  Math.floor(Math.random() * 25) + 1;  
+            break;
+        case 8:
+            chapter =  Math.floor(Math.random() * 25) + 1;  
+            break;
+        case 9:
+            chapter =  Math.floor(Math.random() * 25) + 1;  
+            break;
+        case 10:
+            chapter =  Math.floor(Math.random() * 23) + 1;  
+            break;
+        case 12:
+            chapter =  Math.floor(Math.random() * 21) + 1;  
+            break;
+        case 16:
+            chapter =  Math.floor(Math.random() * 21) + 1;  
+            break;
+        case 19:
+            chapter =  Math.floor(Math.random() * 20) + 1;  
+        break;
+        case 20:
+            chapter =  Math.floor(Math.random() * 21) + 1;  
+        break;
+        case 21:
+            chapter =  Math.floor(Math.random() * 23) + 1;  
+            break;
+        case 29:
+            chapter =  Math.floor(Math.random() * 21) + 1;  
+            break;
+        case 30:
+            chapter =  Math.floor(Math.random() * 23) + 1;  
+            break;
+        default:
+            chapter =  Math.floor(Math.random() * 22) + 1;
+            break;
+    }
+    
+    document.getElementById("slarrauri-random-simpson-chapter-link").href 
+        = "https://pelisplus.me/serie/los-simpsons/temporada-"+season+"/capitulo-"+chapter+"/";
+    
+    document.getElementById("slarrauri-random-simpson-chapter-text").innerHTML
+        = "<b>Temporada:</b> "+season+" <b>Capítulo:</b> "+chapter+"";
 
 }
